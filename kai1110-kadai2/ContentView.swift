46a56076fc6ffb07928bf1805c8b7489384f6fd4
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-11-09 00:24:12 +0900

    Commit

--- kai1110-kadai2/ContentView.swift
+++ kai1110-kadai2/ContentView.swift
@@ -1,21 +1,58 @@
-//
-//  ContentView.swift
-//  kai1110-kadai2
-//
-//  Created by 渡邊魁優 on 2022/11/08.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number1 = ""
+    @State private var number2 = ""
+    @State private var signNumber = 0
+    @State private var result = "0"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            TextField("", text: $number1)
+            TextField("", text: $number2)
+            
+            Picker(selection: $signNumber, label: Text("符号を選択")) {
+                Text("+")
+                    .tag(0)
+                Text("-")
+                    .tag(1)
+                Text("×")
+                    .tag(2)
+                Text("÷")
+                    .tag(3)
+            }
+            .pickerStyle(.segmented)
+            
+            Button(action: {
+                result = calclation()
+            }) {
+                Text("計算")
+            }
+            Text(result)
         }
-        .padding()
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .frame(width: 200)
+    }
+    func calclation() -> String {
+        let num1 = Double(number1) ?? 0
+        let num2 = Double(number2) ?? 0
+        
+        switch signNumber {
+        case 0:
+            return String(num1 + num2)
+        case 1:
+            return String(num1 - num2)
+        case 2:
+            return String(num1 * num2)
+        case 3:
+            guard num2 == 0 else {
+                return String(num1 / num2)
+            }
+            return "割る数には0以外を入力してください"
+        default:
+            return "エラー"
+        }
     }
 }
 
