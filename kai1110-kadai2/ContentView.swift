73d8670600cc5d80e94b0db1635ce808bc625d8e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-11-11 17:21:43 +0900

    フィードバックを反映

--- kai1110-kadai2/ContentView.swift
+++ kai1110-kadai2/ContentView.swift
@@ -1,10 +1,19 @@
 
 import SwiftUI
 
+enum Operation: String, CaseIterable, Identifiable {
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication = "×"
+    case division = "÷"
+    
+    var id: Self { self }
+}
+
 struct ContentView: View {
     @State private var number1 = ""
     @State private var number2 = ""
-    @State private var signNumber = 0
+    @State private var signNumber = Operation.addition
     @State private var result = "0"
     
     var body: some View {
@@ -12,15 +21,21 @@ struct ContentView: View {
             TextField("", text: $number1)
             TextField("", text: $number2)
             
-            Picker(selection: $signNumber, label: Text("符号を選択")) {
-                Text("+")
-                    .tag(0)
-                Text("-")
-                    .tag(1)
-                Text("×")
-                    .tag(2)
-                Text("÷")
-                    .tag(3)
+//            Picker(selection: $signNumber, label: Text("符号を選択")) {
+//                Text("+")
+//                    .tag(0)
+//                Text("-")
+//                    .tag(1)
+//                Text("×")
+//                    .tag(2)
+//                Text("÷")
+//                    .tag(3)
+//            }
+            
+            Picker("Operation", selection: $signNumber) {
+                ForEach(Operation.allCases) { operation in
+                    Text(operation.rawValue)
+                }
             }
             .pickerStyle(.segmented)
             
@@ -39,19 +54,17 @@ struct ContentView: View {
         let num2 = Double(number2) ?? 0
         
         switch signNumber {
-        case 0:
+        case .addition:
             return String(num1 + num2)
-        case 1:
+        case .subtraction:
             return String(num1 - num2)
-        case 2:
+        case .multiplication:
             return String(num1 * num2)
-        case 3:
+        case .division:
             guard num2 == 0 else {
                 return String(num1 / num2)
             }
             return "割る数には0以外を入力してください"
-        default:
-            return "エラー"
         }
     }
 }
